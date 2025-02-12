1be63be07ea96b1d4e20e20f3d976dfc747a6906
Ayman Alhourani <aalhoura@outlook.com>
2025-02-11 21:13:07 -0500

    Separate jobs from featured jobs

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -6,14 +6,15 @@
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="" class="self-start text-sm text-gray-400">Laracasts</a>
-        <h3 class="font-bold text-xl mt-2 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <a href="" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
+        <h3 class="font-bold text-xl mt-2 group-hover:text-blue-600 transition-colors duration-300">{{ $job->title }}
+        </h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div>
         @foreach ($job->tags as $tag)
-            <x-tag :tag="$tag" />
+            <x-tag :$tag />
         @endforeach
     </div>
 </x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,20 +1,20 @@
 @props(['job'])
 
 <x-panel class="flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8">
         <h3
             class="group-hover:text-blue-600 transition-colors duration-300 text-xl transition-colors duration-300 font-bold">
-            Video Producer
+            {{ $job->title }}
         </h3>
-        <p class="text-sm mt-4">Full Time - From $60,000</p>
+        <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
             @foreach ($job->tags as $tag)
-                <x-tag size="small" :tag="$tag" />
+                <x-tag size="small" :$tag />
             @endforeach
         </div>
         <x-employer-logo :width="42" />
